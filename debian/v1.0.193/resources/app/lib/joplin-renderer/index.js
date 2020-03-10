bb1a3e03d25922e72af010802ff922357c276535
initinfosec <initinfosec@protonmail.com>
2020-03-10 15:57:16 -0500

    updating

--- debian/v1.0.193/resources/app/lib/joplin-renderer/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	MarkupToHtml: require('./MarkupToHtml'),
-	MdToHtml: require('./MdToHtml'),
-	HtmlToHtml: require('./HtmlToHtml'),
-	setupLinkify: require('./MdToHtml/setupLinkify'),
-	assetsToHeaders: require('./assetsToHeaders'),
-};
